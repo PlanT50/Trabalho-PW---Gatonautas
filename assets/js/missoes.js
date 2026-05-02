4c3fbb645a4d75a8a9b7a18b9dda28595510001f
PlanT50 <omegadarkxyz@gmail.com>
2026-05-02 17:03:37 -0300

    Função para abrir tela de missões

---
+++ assets/js/missoes.js
@@ -0,0 +1,10 @@
+const missionDelegar = document.getElementById("mission-delegar");
+
+function openMissionPanel(){
+    missionDelegar.classList.add("open");
+    missionDelegar.setAttribute("aria-hidden", "false");
+
+
+}
+
+document
