218dc000e8fe9ec7411f0155917aa7efd3308e93
PlanT50 <omegadarkxyz@gmail.com>
2026-05-03 11:02:43 -0300

    Função de apertar na missão ajeitada e Lista de nomes de gato adicionada

--- assets/js/missoes.js
+++ assets/js/missoes.js
@@ -1,10 +1,16 @@
 const missionDelegar = document.getElementById("mission-delegar");
+const missionPlanet = document.getElementById("mission-Planet");
 
-function openMissionPanel() {
+function openMissionPanel(planet) {
+    missionPlanet.textContent = `Missão escolhida: ${planet}`;
+
   missionDelegar.classList.add("open");
   missionDelegar.setAttribute("aria-hidden", "false");
 }
 
 document.querySelectorAll(".mission-open").forEach(button => {
-  button.addEventListener("click", openMissionPanel);
+  button.addEventListener("click", () => {
+    const planet = button.dataset.planet;
+    openMissionPanel(planet);
+  });
 });
