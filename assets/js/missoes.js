13384f79f32d1b56cb0bdc1f5f51c4ca2bc734d1
PlanT50 <omegadarkxyz@gmail.com>
2026-05-03 11:04:04 -0300

    Lista de gatos mesmo

--- assets/js/missoes.js
+++ assets/js/missoes.js
@@ -1,5 +1,6 @@
 const missionDelegar = document.getElementById("mission-delegar");
 const missionPlanet = document.getElementById("mission-Planet");
+const crew = document.getElementById("Crew");
 
 function openMissionPanel(planet) {
     missionPlanet.textContent = `Missão escolhida: ${planet}`;
@@ -14,3 +15,21 @@ document.querySelectorAll(".mission-open").forEach(button => {
     openMissionPanel(planet);
   });
 });
+
+const gatos = [
+    "Luna Nebulosa",
+    "Pixel Garras",
+    "Tom Vega",
+    "Mika Andrômeda",
+    "Nina Cosmos",
+    "Ziggy Stardust",
+    "Astro Garfield",
+    "Lyra Miau",
+    "Bento Meteoro",
+    "Capitã Amora",
+    "Cookie Saturno",
+    "Sushi Orion",
+    "Mel Quasar",
+    "Tico Lunar",
+    "Nox Eclipse"
+]
